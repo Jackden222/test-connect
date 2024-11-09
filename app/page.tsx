2040f7e3514748a4a600102c23b789f84c2317c0
ASAS <118246454+Jackden222@users.noreply.github.com>
2024-11-09 20:21:30 +0600

    interface

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,13 @@
 
 import { useEffect, useState } from 'react'
 import { WebApp } from '@twa-dev/types'
-import BottomNavigation from './components/ui/BottomNavigation'
+import Hamster from './icons/Hamster';
+import Info from './icons/Info';
+import { binanceLogo, dailyCipher, dailyCombo, dailyReward, dollarCoin, hamsterCoin, mainCharacter } from './images';
+import Settings from './icons/Settings';
+import Mine from './icons/Mine';
+import Friends from './icons/Friends';
+import Coins from './icons/Coins';
 
 declare global {
   interface Window {
@@ -83,21 +89,103 @@ export default function Home() {
   if (!user) return <div className="container mx-auto p-4">Loading...</div>
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Welcome, {user.firstName}!</h1>
-      <p>Your current points: {user.points}</p>
-      <button
-        onClick={handleIncreasePoints}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
-      >
-        Increase Points
-      </button>
+    <div className="bg-black flex justify-center">
+      <div className="w-full bg-black text-white h-screen font-bold flex flex-col max-w-xl">
+        <div className="px-4 z-10">
+          <div className="flex items-center space-x-2 pt-4">
+            <div className="p-1 rounded-lg bg-[#1d2025]">
+              <Hamster size={24} className="text-[#d4d4d4]" />
+            </div>
+            <div>
+              <p className="text-sm">{user.firstName}(CEO)</p>
+            </div>
+          </div>
+          <div className="flex items-center justify-between space-x-4 mt-1">
+            <div className="flex items-center w-1/3">
+              <div className="w-full">
+                <div className="flex justify-between">
+                  <p className="text-sm">Bronze 1</p>
+                  <p className="text-sm">122 <span className="text-[#95908a]">/10</span></p>
+                </div>
+                <div className="flex items-center mt-1 border-2 border-[#43433b] rounded-full">
+                  <div className="w-full h-2 bg-[#43433b]/[0.6] rounded-full">
+                    <div className="progress-gradient h-2 rounded-full"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center w-2/3 border-2 border-[#43433b] rounded-full px-4 py-[2px] bg-[#43433b]/[0.6] max-w-64">
+            <img src={binanceLogo.src} alt="Exchange" className="w-8 h-8" />
+              <div className="h-[32px] w-[2px] bg-[#43433b] mx-2"></div>
+              <div className="flex-1 text-center">
+                <p className="text-xs text-[#85827d] font-medium">Profit per hour</p>
+                <div className="flex items-center justify-center space-x-1">
+                  <img src={dollarCoin.src} alt="Dollar Coin" className="w-[18px] h-[18px]" />
+                  <p className="text-sm">1 200</p>
+                  {/* {formatProfitPerHour(profitPerHour)} */}
+                  <Info size={20} className="text-[#43433b]" />
+                </div>
+              </div>
+              <div className="h-[32px] w-[2px] bg-[#43433b] mx-2"></div>
+              <Settings className="text-white" />
+            </div>
+          </div>
+      </div>
+
+      <div className="flex-grow mt-4 bg-[#f3ba2f] rounded-t-[48px] relative top-glow z-0">
+          <div className="absolute top-[2px] left-0 right-0 bottom-0 bg-[#1d2025] rounded-t-[46px]">
+            <div className="px-4 mt-6 flex justify-between gap-2">
+              <div className="bg-[#272a2f] rounded-lg px-4 py-2 w-full relative">
+                <div className="dot"></div>
+                <img src={dailyReward.src} alt="Daily Reward" className="mx-auto w-12 h-12" />
+                <p className="text-[10px] text-center text-white mt-1">Daily reward</p>
+                <p className="text-[10px] font-medium text-center text-gray-400 mt-2">Time</p>
+              </div>
+              <div className="bg-[#272a2f] rounded-lg px-4 py-2 w-full relative">
+                <div className="dot"></div>
+                <img src={dailyCipher.src} alt="Daily Cipher" className="mx-auto w-12 h-12" />
+                <p className="text-[10px] text-center text-white mt-1">Daily cipher</p>
+                <p className="text-[10px] font-medium text-center text-gray-400 mt-2">Time</p>
+              </div>
+              <div className="bg-[#272a2f] rounded-lg px-4 py-2 w-full relative">
+                <div className="dot"></div>
+                <img src={dailyCombo.src} alt="Daily Combo" className="mx-auto w-12 h-12" />
+                <p className="text-[10px] text-center text-white mt-1">Daily combo</p>
+                <p className="text-[10px] font-medium text-center text-gray-400 mt-2">Time</p>
+              </div>
+            </div>
+
+            <div className="px-4 mt-4 flex justify-center">
+              <div className="px-4 py-2 flex items-center space-x-2">
+                <img src={dollarCoin.src} alt="Dollar Coin" className="w-10 h-10" />
+                <p className="text-4xl text-white">{user.points}</p>
+              </div>
+            </div>
+
+            <div className="px-4 mt-4 flex justify-center">
+              <div
+                className="w-80 h-80 p-4 rounded-full circle-outer"
+                onClick={handleIncreasePoints}
+              >
+                <div className="w-full h-full rounded-full circle-inner">
+                  <img src={mainCharacter} alt="Main Character" className="w-full h-full" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+      {/* <p>Your current points: {user.points}</p>
+      <button onClick={handleIncreasePoints}className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Increase Points</button> */}
+
       {notification && (
         <div className="mt-4 p-2 bg-green-100 text-green-700 rounded">
           {notification}
         </div>
       )}
-      <BottomNavigation/>
+          
+      </div>
     </div>
   )
 }
--- app/page/Friennds.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Friennds = () => {
-  return (
-    <div>Friennds</div>
-  )
-}
-
-export default Friennds
